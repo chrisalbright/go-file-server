76226a6098607fbe9967a4e9d9c21e1b9503637a
Chris Albright <calbright@cj.com>
2015-06-03 18:39:47 -0700

    first

---
+++ fileserve.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "net/http"
+  "flag"
+  "strconv"
+  "fmt"
+)
+
+func main() {
+  var (
+    address = flag.String("listen", "", "address to bind")
+    port = flag.Int("port", 8080, "port to listen")
+    path = flag.String("path", ".", "path to serve")
+    host = *address + ":" + strconv.Itoa(*port)
+  )
+  flag.Parse()
+  fmt.Printf("Serving %s at %s", *path, host)
+  http.ListenAndServe(host, http.FileServer(http.Dir(*path)))
+}
